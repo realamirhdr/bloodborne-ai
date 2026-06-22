b95b90caa9f57e519ae76c2c3cd3291058853758
Amirreza Heidari <lithiumhdr@gmail.com>
2026-06-22 18:11:05 -0400

    Add: added cors

--- backend/src/api/main.py
+++ backend/src/api/main.py
@@ -32,7 +32,13 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "https://amirheidari.com",
+        "https://www.amirheidari.com",
+        "https://bloodborne-ai.amirheidari.com",
+    ],
     allow_methods=["POST", "GET"],
     allow_headers=["Content-Type"],
 )
